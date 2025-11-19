f71adbe18f33f066eff3ae769c72d15951c1a607
eligifty-coder <gift.weike@gmail.com>
2025-11-19 20:19:56 +0000

    php_build_query and creating parameter dynamically from  html links

---
+++ 06_url_parameters/51_dynamic_url_creation.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/simple.css" />
+    <link rel="stylesheet" href="../styles/custom.css" />
+    <title>Document</title>
+<body>
+    <pre>
+        <?php
+        var_dump($_GET);
+        ?>
+        <!-- <a href="51_dynamic_url_creation.php?book=Harry Potter"> Harry Potter</a>
+        <a href="51_dynamic_url_creation.php?book=Beauty & the Beast"> Beauty & the Beast</a> -->
+        <a href="51_dynamic_url_creation.php?<?php echo http_build_query(['book'=> 'harry potter']);?>"> Harry Potter</a>
+        <a href="51_dynamic_url_creation.php?<?php echo http_build_query(['book'=> 'beauty & the beast']);?>"> Harry Potter</a>
+    </pre>
+</body>
+</html>     
