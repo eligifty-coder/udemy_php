c560534db3ba6d48d1aee0c916699b9255ddb94c
eligifty-coder <gift.weike@gmail.com>
2025-11-19 15:48:42 +0000

    best practice with associative array

---
+++ 05_associative_array/47_bestPractice_in_associative_array.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/simple.css" />
+    <link rel="stylesheet" href="../styles/custom.css" />
+    <title>Document</title>
+<body>
+    <pre>
+        <?php
+        $books = [
+            'harry potter' => 'j.k rowling',
+            'lord of the rings' => 'j.r.r tolkien',
+            'the little prince' => 'antoine de saint-exupery',
+            'don quixote' => 'miguel de cervantes',
+            'alice in wonderland' => 'lewis carroll',
+            0 => "This is book #0",
+            "another book",
+        ];
+
+        var_dump($books);
+        var_dump($books['0']);
+        $books['0'] = "overwritten book";
+        var_dump($books[0] );
+        /** Don't mix associative array and normal array
+        
+         */
+        
+        ?>
+    </pre>
+</body>
+</html>     
