63af06e7f2382eb40db217a7ea7096fd90b1a475
eligifty-coder <gift.weike@gmail.com>
2025-12-04 18:08:10 +0000

    file inclusion

---
+++ 08_advanced_concept/70_file_inclusion/require.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type='text/css' href="../../styles//simple.css">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        <?php
+        require_once __DIR__ . '/inc/functions.inc.php';
+        $text = 'PHP is amazing!';
+        ?>
+        
+    </pre>
+    <h1><?php echo e($text);?></h1>
+</body>
+</html>
