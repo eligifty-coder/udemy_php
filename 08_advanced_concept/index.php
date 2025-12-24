7d3719eba724603eb23ae405b1780ede1bf11e46
eligifty-coder <gift.weike@gmail.com>
2025-12-24 14:37:13 +0000

    define nested array

--- 08_advanced_concept/index.php
+++
@@ -1,3 +0,0 @@
-if there is an index.html fime in a folder, php display the file instead of opening the folder up in the browser
-if there is an index.php and index.html file, the browser picks up the index.html file.
-index.php gets picked up and opens when index.html is absent
