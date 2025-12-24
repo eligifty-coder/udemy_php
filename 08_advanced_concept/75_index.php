66faeb4775a586e48b07a9183594441b1814f999
eligifty-coder <gift.weike@gmail.com>
2025-12-24 13:40:43 +0000

    updated file name

---
+++ 08_advanced_concept/75_index.php
@@ -0,0 +1,3 @@
+if there is an index.html fime in a folder, php display the file instead of opening the folder up in the browser
+if there is an index.php and index.html file, the browser picks up the index.html file.
+index.php gets picked up and opens when index.html is absent
