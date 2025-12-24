283c79c3b0edd12833791387f9500735fcf8f9ce
eligifty-coder <gift.weike@gmail.com>
2025-12-24 18:11:25 +0000

    accessing multi level array details with foreacg

---
+++ 09_nested_arrays/79_multi_level_array_with_foreach.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="css/type" href="styles/simple.css">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        <?php
+        require_once '../inc/xss.inc.php';
+
+        $courses = [
+            [
+                'title' =>'German for Beginners',
+                'desc' => 'Learn basic German vocabulary, grammar, and everyday phrases.',
+                'flag' => '🇩🇪',
+                'topics' => [
+                    'Introduction to German Alphabet and Sounds',
+                    'Basic Greetings and Farewells',
+                    'Numbers and Counting',
+                    'Common Verbs and Basic Sentence Structures',
+                    'Everyday Phrases for Simple Conversations',
+                    'Asking and Giving Directions',
+                    'Food and Dining Vocabulary',
+                    'Basic Grammar Rules: Articles and Cases'
+                ],
+            ],
+            
+            [
+                'title' => 'French for Beginners',
+                'desc' => 'Master fundamental French skills including basic vocabulary and conversational techniques.',
+                'flag' => '🇫🇷',
+                'topics' => [
+                    'Basics of French Pronunciation',
+                    'Introducing Yourself and Others',
+                    'Numbers, Time, and Dates',
+                    'Everyday Phrases for General Conversations',
+                    'Basic French Grammar: Subject-Verb Agreement',
+                    'Travel-Related Vocabulary and Phrases',
+                    'Food, Drinks, and Dining Out',
+                    'Clothing and Shopping Vocabulary'
+                ]
+            ],
+
+            [
+                'title' => 'Spanish for Beginners',
+                'desc' => 'Acquire essential Spanish vocabulary and gain proficiency in daily conversations.',
+                'flag' => '🇪🇸',
+                'topics' => [
+                    'Spanish Alphabets and Sounds',
+                    'Basic Greetings and Introductions',
+                    'Numbers and Basic Mathematics',
+                    'Essential Phrases for Everyday Use',
+                    'Common Verbs and Their Conjugations',
+                    'Navigational Vocabulary: Directions and Locations',
+                    'Basic Food Vocabulary and Ordering at a Restaurant',
+                    'Understanding Gender and Articles in Spanish'
+                ]
+            ],
+        ];
+        $spanishCourse =  $courses[2];
+        foreach($spanishCourse['topics'] AS $topic){
+            var_dump($topic);
+            echo '<br>';
+        }
+        ?>
+    </pre>
+    <?php foreach($courses AS $course): ?>
+        <details>
+            <summary>
+                <?php echo e($course['flag']); ?> 
+                <?php echo e($course['title']); ?> 
+            </summary>
+            <p> <?php echo e($course['desc']); ?>  </p>
+            <ul>
+                <?php foreach($course['topics'] AS $topic) :?>
+                    <li> <?php echo $topic ;?> </li>
+                <?php endforeach; ?>
+            </ul>
+        </details>
+    <?php endforeach; ?>
+
+</body>
+</html>
